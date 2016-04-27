b31a53c970ded93e5689ee9f033bb9faaea3e051
popescu95 <popescu.alexandru95@gmail.com>
2016-04-27 23:34:58 +0300

    NeuralNetwork
    Implement NeuralNetwork

--- modulML/src/modulml/NeuralNetwork.java
+++ modulML/src/modulml/NeuralNetwork.java
@@ -1,109 +1,133 @@
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NeuralNetwork {
 
-    /**
-   * 
-   * @element-type Neuron
-   */
-  public Vector  myNeuron;
-    /**
-   * 
-   * @element-type Neuron
-   */
-  public Vector  myNeuron;
-    /**
-   * 
-   * @element-type Layer
-   */
-  public Vector  layers;
-    public Vector  myLayer;
-    public Vector  myLearningRule;
-    public Vector  myLearningRule;
+  private List<Neuron>  outputNeurons;
+
+  private List<Neuron>  inputNeurons;
+
+  private List<Layer>  layers;
+  private LearningRule  learningRule;
 
   public void NeuralNetwork() {
+    outputNeurons = new ArrayList<>();
+    inputNeurons = new ArrayList<>();
+    layers = new ArrayList<>();
   }
 
   public void addLayer(Layer layer) {
+    final boolean ok;
+    ok = layers.add(layer);
   }
 
   public void addLayer(int idx, Layer layer) {
+    final boolean ok;
+    layers.add(idx,layer);
   }
 
   public void removeLayer(Layer layer) {
+    final boolean rmv;
+    rmv = layers.remove(layer);
   }
 
   public void removeLayer(int idx) {
+    Layer layer = layers.remove(idx);
   }
 
-  public Iterator<Layer> getLayersIterator() {
-  return null;
-  }
+    /*public Iterator<Layer> getLayersIterator() {
+        return null;
+    }*/
 
   public List<Layer> getLayers() {
-  return null;
+    return layers;
   }
 
-  public void setInput(List<double> inputVector) {
+  public void setInput(List<Double> inputVector) {
+    for(int i=0;i<inputVector.size();i++)
+      inputNeurons.get(i).setInput(inputVector.get(i));
   }
 
-  public void setInput() {
-  }
+    /*public void setInput() {
 
-  public List<double> getOutput() {
-  return null;
+    }*/
+
+  public List<Double> getOutput() {
+    List<Double> outputList = new ArrayList<>();
+    for(int i=0;i<outputNeurons.size();i++)
+      outputList.add(outputNeurons.get(i).getOutput());
+    return outputList;
   }
 
   public void calculate() {
+    for(int i=0;i<layers.size();i++)
+      layers.get(i).calculate();
   }
 
   public void reset() {
+    for(int i=0;i<layers.size();i++)
+      layers.get(i).reset();
   }
 
   public void run() {
   }
 
   public void learn(TrainingSet trainingSetToLearn) {
+    learningRule.setNeuralNetwork(this);
+    learningRule.learn(trainingSetToLearn);
   }
 
   public void learn(TrainingSet trainingSetToLearn, LearningRule learningRule) {
+    learningRule.setNeuralNetwork(this);
+    learningRule.learn(trainingSetToLearn);
   }
 
   public void stopLearning() {
+    learningRule.stopLearning();
   }
 
   public void randomizeWeights() {
+    for(int i=0;i<layers.size();i++)
+      layers.get(i).randomizeWeights();
   }
 
   public List<Neuron> getInputNeurons() {
-  return null;
+    return inputNeurons;
   }
 
-  public void setInputNeurons(List<Neuron> inputNeurons) {
+  public void setInputNeurons(List<Neuron> inputNeuronsList) {
+    for(int i=0;i<inputNeuronsList.size();i++)
+      inputNeurons.add(inputNeuronsList.get(i));
   }
 
   public List<Neuron> getOutputNeurons() {
-  return null;
+    return outputNeurons;
   }
 
-  public void setOutputNeurons(List<Neuron> outputNeurons) {
+  public void setOutputNeurons(List<Neuron> outputNeuronsList) {
+    for(int i=0;i<outputNeuronsList.size();i++)
+      outputNeurons.add(outputNeuronsList.get(i));
   }
 
   public LearningRule getLearningRule() {
-  return null;
+    return learningRule;
   }
 
-  public void setLearningRule(LearningRule learningRule) {
+  public void setLearningRule(LearningRule learningRuleToSet) {
+    learningRule = learningRuleToSet;
   }
 
   public void notifyChange() {
+    learningRule.notifyChange();
   }
 
   public void createConnection(Neuron fromNeuron, Neuron toNeuron, double weightVal) {
+    Connection connection = new Connection(fromNeuron, toNeuron);
+    connection.getWeight().setValue(weightVal);
   }
 
-  public String toString() {
-  return null;
-  }
+    /*public String toString() {
+        return null;
+    }*/
 
 }
