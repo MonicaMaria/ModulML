905fb841e83a96626d91d3fe9d201cc4c8d7ecf9
Monica <vizitiu.monica@gmail.com>
2016-04-28 00:13:48 +0300

    main

--- modulML/src/modulml/ModulML.java
+++ modulML/src/modulml/ModulML.java
@@ -5,6 +5,9 @@
  */
 package modulml;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Monica
@@ -16,6 +19,23 @@ public class ModulML {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        NeuralNetwork retea = new NeuralNetwork();
+        List<Double> input = new ArrayList<>();
+        for(int i=0;i<30;i++) input.add(0.0);
+        input.set(1, 1.0);
+        input.set(9, 1.0);
+        List<Neuron> inputNeurons = new ArrayList<>();
+        inputNeurons.add(new Neuron());
+        inputNeurons.add(new Neuron());
+        inputNeurons.add(new Neuron());
+        retea.setInputNeurons(inputNeurons);
+        retea.setInput(input);
+        Layer hid = new Layer();
+        retea.addLayer(hid);
+        hid.addNeuron(new Neuron());
+        hid.addNeuron(new Neuron());
+        
+        retea.run();
     }
     
 }
