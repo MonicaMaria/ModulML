4e8912e006cc344a240ef7c933cd353011618cd2
georgianadinu <georgiana_dinu1@yahoo.com>
2016-04-28 08:42:11 +0300

    transferfunction

--- modulML/src/modulml/TransferFunction.java
+++ modulML/src/modulml/TransferFunction.java
@@ -2,26 +2,35 @@ package modulml;
 
 public class TransferFunction {
 
-  protected double output;
-   
-  public double getOutput(double net) {
-      return output;
-  }
-  
-  public double activate(double net) {
-    if (net>0.5)
-        return 1.0;
-    else return 0.0;
-  }
+    protected double output;
+    private double slope = 1d;
 
-  public double getDerivative(double net) {
-    return 0.0;
-  }
+    public double getOutput(double net) {
+        return output;
+        
+    }
 
-  @Override
-  public String toString() {
-    return getClass().getName();
-  }
+    public double activate(double net) {
+        if (net > 100) {
+            return 1.0;
+        } else if (net < -100) {
+            return 0.0;
+        }
 
-    
+        double den;
+        den = 1d + Math.exp(-this.slope * net);
+        this.output = (1d / den);
+
+        return this.output;
+    }
+
+    public double getDerivative(double net) {
+        return 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName();
+    }
+
 }
